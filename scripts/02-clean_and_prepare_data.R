fc12ab0c3df8f9d5ef6ac29ec76ee5a878ca7865
Tianyi Zhang <tianyi.zhang6546@gmail.com>
2022-04-03 12:05:47 -0400

    Modified 02-clean_and_prepare_data.R to also create a dataset that contains the variable 'province', stored in outputs/data/clean_data_province.csv.

--- scripts/02-clean_and_prepare_data.R
+++ scripts/02-clean_and_prepare_data.R
@@ -20,17 +20,34 @@ kenya_condom_clean <- kenya_condom %>%
   clean_names()
 
 #### Data cleanup and prepare ####
-# Add a new column that denotes the gender
-kenya_condom_clean <- kenya_condom_clean %>%
-  mutate(gender = ifelse(as.numeric(rownames(kenya_condom_clean))<28,"female","male"))
-kenya_condom_clean <- kenya_condom_clean[,c(1,11,2:10)]
-# We will filter to only the age rows and rename the var column
-kenya_condom_clean <- kenya_condom_clean[c(2:6,27,29:34,55),] %>%
-  rename(age = var)
+# There are multiple variables such as age, residence, province, education, etc.
+# As such, the raw data will be split into two separate datasets that each focus
+# on either age or province, the two main variables.
 
+## Age dataset ##
+# Add a new column that denotes the gender
+kenya_condom_age <- kenya_condom_clean %>%
+  mutate(gender = ifelse(as.numeric(rownames(kenya_condom_clean))<28,"female","male"))
+kenya_condom_age <- kenya_condom_age[,c(1,11,2:10)]
+# We will filter to only the age rows and rename the var column
+kenya_condom_age <- kenya_condom_age[c(2:6,27,29:34,55),] %>%
+  rename(age = var)
 # Now we change the class of the variables
-kenya_condom_clean <- kenya_condom_clean %>%
+kenya_condom_age <- kenya_condom_age %>%
   mutate(age = as.factor(age)) %>%
+  mutate(gender = as.factor(gender))
+
+## Province dataset ##
+# Add a new column that denotes the gender
+kenya_condom_province <- kenya_condom_clean %>%
+  mutate(gender = ifelse(as.numeric(rownames(kenya_condom_clean))<28,"female","male"))
+kenya_condom_province <- kenya_condom_province[,c(1,11,2:10)]
+# We will filter to only the province rows and rename the var column
+kenya_condom_province <- kenya_condom_province[c(15:21,27,43:49,55),] %>%
+  rename(province = var)
+# Now we change the class of the variables
+kenya_condom_province <- kenya_condom_province %>%
+  mutate(province = as.factor(province)) %>%
   mutate(gender = as.factor(gender))
 
 #### Dataset tests ####
@@ -38,8 +55,10 @@ kenya_condom_clean <- kenya_condom_clean %>%
 # between 0 and 100, and that number of people is greater than 0
 # Could maybe also test that the number of people for each gender
 # adds up to the total? (don't know how to T_T)
+
+## Age dataset tests ##
 agent <-
-  create_agent(tbl = kenya_condom_clean) %>%
+  create_agent(tbl = kenya_condom_age) %>%
   col_is_factor(columns = vars(age, gender)) %>%
   col_is_numeric(columns = vars(public_sector,
                                 private_medical_sector,
@@ -68,3 +87,40 @@ agent <-
   interrogate()
 
 agent
+
+## Province dataset tests ##
+agent2 <-
+  create_agent(tbl = kenya_condom_province) %>%
+  col_is_factor(columns = vars(province, gender)) %>%
+  col_is_numeric(columns = vars(public_sector,
+                                private_medical_sector,
+                                private_pharmacy,
+                                shop,
+                                cbd_agent,
+                                friends_and_relatives,
+                                other_sources,
+                                dont_know_a_source,
+                                number_of_people)) %>%
+  col_vals_in_set(columns = province,
+                  set = c("Nairobi","Central","Coast","Eastern",
+                          "Nyanza","Rift Valley","Western","Total")) %>%
+  col_vals_in_set(columns = gender,
+                  set = c("male", "female")) %>%
+  col_vals_between(columns = vars(public_sector,
+                                  private_medical_sector,
+                                  private_pharmacy,
+                                  shop,
+                                  cbd_agent,
+                                  friends_and_relatives,
+                                  other_sources,
+                                  dont_know_a_source),
+                   left = 0,
+                   right = 100) %>%
+  col_vals_gte(columns = number_of_people, value = 0) %>%
+  interrogate()
+
+agent2
+
+#### Save data ####
+write_csv(kenya_condom_age, "outputs/data/cleaned_data_age.csv")
+write_csv(kenya_condom_province, "outputs/data/cleaned_data_province.csv")
